916e3be4228a1528375544845e14a889706e20da
groy <gwendal.roy@etudiant.univ-brest.fr>
2023-01-23 19:25:55 +0100

    Création de la vue Inscription

---
+++ views/users/inscription.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Chat libre</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <link rel="stylesheet" href="./lib/css/styles.css">
+    </head>
+
+    <body class="d-flex align-items-center">
+        
+        <div class="container">
+            <div class="jumbotron">
+                <h1 class="display-4">Inscription</h1>
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="username">Nom d'utilisateur</label>
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Entrez un nom d'utilisateur">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Mot de passe</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Entrez un mot de passe">
+                    </div>
+                    <button type="submit" class="btn btn-primary">S'inscrire</button>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
