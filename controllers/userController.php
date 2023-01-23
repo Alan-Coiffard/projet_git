a58a041882767cec6efe2100bcfd05f69f96b1ff
Alan <alan.coiffar@gmail.com>
2023-01-23 12:42:54 +0100

    View & debut controller

--- controllers/userController.php
+++ controllers/userController.php
@@ -6,4 +6,18 @@ class UserController {
     public function index() {
         require_once 'views/users/index.php';
     }
+
+    public function login() {
+        if(isset($_POST['username'])  && isset($_POST['password'])) {
+            if(!empty($_POST['username']) && !empty($_POST['password'])) {
+                echo "<p class='msg_valid'>Données envoyées</p>";
+                
+            } else {
+                echo "<p class='msg_nvalid'>Données non envoyées, veuillez completer tous le champs</p>";
+            }
+            require_once 'views/users/login.php';
+        } else {
+            require_once 'views/users/login.php';
+        }
+    }
 }
