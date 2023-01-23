6c442e3c72bb18ce9205e190b091713b1b886c73
Alan <alan.coiffar@gmail.com>
2023-01-23 15:28:50 +0100

    Conexion - controller & model

--- controllers/userController.php
+++ controllers/userController.php
@@ -7,15 +7,21 @@ class UserController {
         require_once 'views/users/index.php';
     }
 
+    public function chat() {
+        require_once 'views/users/chat.php';
+    }
+
     public function login() {
         if(isset($_POST['username'])  && isset($_POST['password'])) {
             if(!empty($_POST['username']) && !empty($_POST['password'])) {
-                echo "<p class='msg_valid'>Données envoyées</p>";
-                
+                if(getConnexion($_POST['username'], $_POST['password'])) {
+                    echo "<p class='msg_valid'>Connexion validée</p>";
+                    header('Location: index.php?controller=user&action=chat');
+                }
             } else {
                 echo "<p class='msg_nvalid'>Données non envoyées, veuillez completer tous le champs</p>";
+                require_once 'views/users/login.php';
             }
-            require_once 'views/users/login.php';
         } else {
             require_once 'views/users/login.php';
         }
